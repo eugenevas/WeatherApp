7225c0fdc5c2b168f9be18008c14862d54aa2622
Evgeniy Vasilyev <eugene.vas@yandex.ru>
2022-09-30 10:51:56 +0500

    Creating repo

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -5,13 +5,21 @@
 //  Created by Евгений Васильев on 29.09.2022.
 //
 
+
+
+// Location: CoreLocation
+// TableView
+// Custom cell: CollectionView
+// API / Request to get the data
+
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
